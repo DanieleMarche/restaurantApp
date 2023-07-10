5fc36fe95fd20cacda7a6dc890346a81bbe40383
Daniele Marchetilli <marchetilli@gmail.com>
2023-07-11 01:04:19 +0200

    chenged the visual of the eventiEpromozioni page

--- lib/pages/eventiEPromozioni.dart
+++ lib/pages/eventiEPromozioni.dart
@@ -1,21 +1,23 @@
 import 'package:flutter/material.dart';
 
 class EventiEPromozioni extends StatelessWidget {
+  const EventiEPromozioni({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Eventi e Promozioni'),
+        title: const Text('Eventi e Promozioni'),
         centerTitle: true,
       ),
       body: Center(
         child: TextButton(
-          child: Text('Torna alla Home'),
           onPressed: () => Navigator.pop(context),
           style: TextButton.styleFrom(
               backgroundColor: Colors.white,
-              textStyle: TextStyle(color: Colors.black)
-              )
+              textStyle: const TextStyle(color: Colors.black)
+              ),
+          child: const Text('Torna alla Home')
         ),
       ),
     );
